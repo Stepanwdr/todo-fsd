0e199c12f58c64e7ded032d51fce8199c66bfb19
sma <stepan.manukyan@codedunes.com>
2025-07-22 23:46:25 +0400

    fix: api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import '../app/styles/index.css'
 import { QueryClientProvider } from '@tanstack/react-query';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { queryClient } from 'shared/config';
-import { withToast } from "./providers";
 import { ToastContainer } from "react-toastify";
 
 const HomePage = lazy(() => import('pages/Home'))
@@ -27,4 +26,4 @@ const App = () => (
   </QueryClientProvider>
 );
 
-export default withToast(App)
+export default App
